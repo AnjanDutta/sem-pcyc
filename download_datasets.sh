f310c860ef96c1d7ac0095ec3f86e3cf194ce6c5
AnjanDutta <duttanjan@gmail.com>
2019-04-27 12:36:58 +0200

    probable bug fix

--- download_datasets.sh
+++ download_datasets.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#! /bin/sh
 
 parse_section()
 {
@@ -14,7 +14,9 @@ parse_section()
   done
 }
 path_dataset=$(parse_section $HOSTNAME path_dataset < ./config.ini)
-
+if [ ! -d $path_dataset ]; then
+  mkdir $path_dataset
+fi
 ############################################# download the Sketchy dataset #############################################
 python3 src/download_gdrive.py 0B7ISyeE8QtDdTjE1MG9Gcy1kSkE $path_dataset/Sketchy.7z
 7z $path_dataset/Sketchy.7z
