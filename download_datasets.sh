730aa47729d2f4999e1c00f043befcd2ee1ab30a
AnjanDutta <duttanjan@gmail.com>
2019-04-27 14:44:32 +0200

    further changes in download dataset script

--- download_datasets.sh
+++ download_datasets.sh
@@ -22,10 +22,13 @@ echo "Downloading the Sketchy dataset (it will take some time)"
 python3 src/download_gdrive.py 0B7ISyeE8QtDdTjE1MG9Gcy1kSkE $path_dataset/Sketchy.7z
 7z x $path_dataset/Sketchy.7z -o$path_dataset
 mv $path_dataset/256x256 $path_dataset/Sketchy
+rm $path_dataset/Sketchy.7z
 echo "Downloading the extended photos of Sketchy dataset (it will take some time)"
 python3 src/download_gdrive.py 0B2U-hnwRkpRrdGZKTzkwbkEwVkk $path_dataset/Sketchy/extended_photo.zip
 echo "Unzipping it"
-unzip $path_dataset/Sketchy/extended_photo.zip
+unzip $path_dataset/Sketchy/extended_photo.zip -d $path_dataset/Sketchy
+rm $path_dataset/Sketchy/extended_photo.zip
+mv $path_dataset/Sketchy/EXTEND_image_sketchy $path_dataset/Sketchy/extended_photo
 ############################################ download the TU-Berlin dataset ############################################
 if [ ! -d $path_dataset/TU-Berlin ]; then
   mkdir $path_dataset/TU-Berlin
@@ -33,7 +36,8 @@ fi
 echo "Downloading the sketches of TU-Berlin dataset (it will take some time)"
 wget http://cybertron.cg.tu-berlin.de/eitz/projects/classifysketch/sketches_png.zip -O $path_dataset/TU-Berlin/sketches.zip
 echo "Unzipping it"
-unzip $path_dataset/TU-Berlin/sketches.zip
+unzip $path_dataset/TU-Berlin/sketches.zip -d $path_dataset/TU-Berlin
+mv $path_dataset/TU-Berlin/png $path_dataset/TU-Berlin/sketches
 echo "Downloading the images of TU-Berlin dataset (it will take some time)"
 python3 src/download_gdrive.py 0B2U-hnwRkpRrMFVvTmFQa3dmSUk $path_dataset/TU-Berlin/images.zip
 echo "Unzipping it"
@@ -43,6 +47,10 @@ if [ ! -d $path_dataset/QuickDraw ]; then
   mkdir $path_dataset/QuickDraw
 fi
 echo "Downloading the sketches of QuickDraw dataset (it will take some time)"
-wget http://datasets.cvc.uab.es/QuickDraw/QuickDraw_sketches_final.zip
+wget http://datasets.cvc.uab.es/QuickDraw/QuickDraw_sketches_final.zip $path_dataset/QuickDraw/QuickDraw_sketches_final.zip
+unzip $path_dataset/QuickDraw/QuickDraw_sketches_final.zip
+mv $path_dataset/QuickDraw/QuickDraw_sketches_final $path_dataset/QuickDraw/sketches
 echo "Downloading the images of QuickDraw dataset (it will take some time)"
-wget http://datasets.cvc.uab.es/QuickDraw/QuickDraw_images_final.zip
+wget http://datasets.cvc.uab.es/QuickDraw/QuickDraw_images_final.zip $path_dataset/QuickDraw/QuickDraw_images_final.zip
+unzip $path_dataset/QuickDraw/QuickDraw_images_final.zip
+mv $path_dataset/QuickDraw/QuickDraw_images_final $path_dataset/QuickDraw/QuickDraw_images
